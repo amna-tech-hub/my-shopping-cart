cf510025ae1502ad9dd3474e8f8f5d344d4e64c0
amna-tech-hub <m.amna.dev@gmail.com>
2026-05-25 04:00:43 -0700

    fix case sensitivity issue

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import fetchProductReducer from './slices/fetchProduct';
 import  productCategoryReducer  from './slices/productCategory';
-import  categoryReducer  from './slices/Category';
+import  categoryReducer  from './slices/category';
 import favouritesReducer from './slices/favourite';
 import cartItemReducer from './slices/cartItem';
 export const store = configureStore({
